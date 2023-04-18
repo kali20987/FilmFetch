4a04424722c81b5ff7a5f37e0be02a7638ab696a
Koi-AS <lightningblade.901@gmail.com>
2023-04-18 11:35:12 -0400

    Added some basic pseudocode to the JavaScript

--- search-page-assets/js/search-page.js
+++ search-page-assets/js/search-page.js
@@ -1,3 +1,24 @@
 // Beginning of Search Page js
 
-console.log('hello world')
+console.log('hello world');
+
+var omdbKey = 'http://www.omdbapi.com/?i=tt3896198&apikey=2b24b8e5';
+// var tmdbKey = '';
+
+// Pseudocode
+// ~~~~~~~~~~~~~~~~~~~~~~~
+// Api =
+// After search result input
+// Set search results as variable
+// Use variable as a query parameter in api key searches
+
+// Search results =
+// call data from api keys
+// create dynamic cards
+// add information to cards
+// append cards to section id="search-results"
+
+// Filter results =
+// Create checkbox aside for filters
+// Draw filter data as variable
+// Use filter variables in api key
