1aad8fce808f32a00efb556a61cbac2bcbca0515
Koi-AS <lightningblade.901@gmail.com>
2023-04-19 13:27:33 -0400

    Movie information now displays onto the page. The information dynamically changes depending on the movie that is selected

--- movie-info-assets/js/movie-info.js
+++ movie-info-assets/js/movie-info.js
@@ -1,5 +1,9 @@
 console.log('hello');
 
+var titleEl = document.getElementById('movie-title');
+var posterEl = document.getElementById('movie-poster');
+var overviewEl = document.getElementById('movie-overview');
+
 var tmdbKey = '2a7d4b5715a1c1e68c7c7be6c0b35221';
 
 function getMovieName() {
@@ -21,5 +25,9 @@ function displayResults(movieId) {
             return response.json();
         }).then(function(data) {
             console.log(data);
+            titleEl.textContent = data.title;
+            overviewEl.textContent = data.overview;
+
+            posterEl.setAttribute('src', 'https://image.tmdb.org/t/p/w500' + data.backdrop_path);
         });
 };
