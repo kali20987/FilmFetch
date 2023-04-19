01e48f227cb1daf7e4813c77bfeb9e13b7caa14a
Koi-AS <lightningblade.901@gmail.com>
2023-04-19 13:20:34 -0400

    Information for the selected movie now displays in console log

--- movie-info-assets/js/movie-info.js
+++ movie-info-assets/js/movie-info.js
@@ -2,19 +2,12 @@ console.log('hello');
 
 var tmdbKey = '2a7d4b5715a1c1e68c7c7be6c0b35221';
 
-// fetch('https://api.themoviedb.org/3/movie/' + movieId + '?api_key=' + tmdbKey + '&language=en-US&region=US&')
-//         .then(function(response) {
-//             return response.json();
-//         }).then(function(data) {
-//             console.log(data);
-//         });
-
 function getMovieName() {
     var urlString = document.location.search;
     var movieId = urlString.split('=')[1];
 
     if(movieId) {
-        displayResults();
+        displayResults(movieId);
     } else {
         document.location.replace('../search-page-assets/search-page.html')
     }
@@ -22,6 +15,11 @@ function getMovieName() {
 
 getMovieName()
 
-function displayResults() {
-
+function displayResults(movieId) {
+    fetch('https://api.themoviedb.org/3/movie/' + movieId + '?api_key=' + tmdbKey + '&language=en-US&region=US&')
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
+        });
 };
